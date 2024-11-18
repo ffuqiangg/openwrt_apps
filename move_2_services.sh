2fe79812c87974cc5df0165288c66d819413d1c1
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2024-11-18 12:02:48 +0800

    Update move_2_services.sh

--- move_2_services.sh
+++ move_2_services.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-local lua_file="$({ find |grep "\.lua\|\.htm\|\.json"; } 2>"/dev/null")"
+lua_file="$({ find | grep "\.lua\|\.htm\|\.json"; } 2>"/dev/null")"
 for a in ${lua_file}
 do
     [ -n "$(grep '"vpn"' "$a")" ] && sed -i 's,"vpn","services",g' "$a"
     [ -n "$(grep '"VPN"' "$a")" ] && sed -i 's,"VPN","Services",g' "$a"
     [ -n "$(grep '\[\[vpn\]\]' "$a")" ] && sed -i 's,\[\[vpn\]\],\[\[services\]\],g' "$a"
     [ -n "$(grep 'admin/vpn' "$a")" ] && sed -i 's,admin/vpn,admin/services,g' "$a"
-done
-
-local resource_file="$({ find | grep "\.lua\|\.htm\|\.json"; } 2>"/dev/null")"
-for b in ${resource_file}
-do
-    [ -n "$(grep "\"nas\"" "$b")" ] && sed -i "s,\"nas\",\"services\",g" "$b"
-    [ -n "$(grep "\"NAS\"" "$b")" ] && sed -i "s,\"NAS\",\"Services\",g" "$b"
-    [ -n "$(grep "\[\[nas\]\]" "$b")" ] && sed -i "s,\[\[nas\]\],\[\[services\]\],g" "$b"
-    [ -n "$(grep "admin/nas" "$b")" ] && sed -i "s,admin/nas,admin/services,g" "$b"
+    [ -n "$(grep "\"nas\"" "$a")" ] && sed -i "s,\"nas\",\"services\",g" "$a"
+    [ -n "$(grep "\"NAS\"" "$a")" ] && sed -i "s,\"NAS\",\"Services\",g" "$a"
+    [ -n "$(grep "\[\[nas\]\]" "$a")" ] && sed -i "s,\[\[nas\]\],\[\[services\]\],g" "$a"
+    [ -n "$(grep "admin/nas" "$a")" ] && sed -i "s,admin/nas,admin/services,g" "$a"
 done
 
 exit 0
