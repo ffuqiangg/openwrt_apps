659be49c807e866fb6b1da0d13ee37ebb56e28b0
ffuqiangg <34062439+ffuqiangg@users.noreply.github.com>
2024-11-18 11:54:40 +0800

    Update move_2_services.sh

--- move_2_services.sh
+++ move_2_services.sh
@@ -1,31 +1,21 @@
 #!/bin/bash
-# From: QiuSimons
 
-lua_file="$({ find |grep "\.lua"; } 2>"/dev/null")"
+local lua_file="$({ find |grep "\.lua\|\.htm\|\.json"; } 2>"/dev/null")"
 for a in ${lua_file}
 do
-	[ -n "$(grep '"vpn"' "$a")" ] && sed -i 's,"vpn","services",g' "$a"
-	[ -n "$(grep '"VPN"' "$a")" ] && sed -i 's,"VPN","services",g' "$a"
-	[ -n "$(grep '\[\[vpn\]\]' "$a")" ] && sed -i 's,\[\[vpn\]\],\[\[services\]\],g' "$a"
-	[ -n "$(grep 'admin/vpn' "$a")" ] && sed -i 's,admin/vpn,admin/services,g' "$a"
+    [ -n "$(grep '"vpn"' "$a")" ] && sed -i 's,"vpn","services",g' "$a"
+    [ -n "$(grep '"VPN"' "$a")" ] && sed -i 's,"VPN","Services",g' "$a"
+    [ -n "$(grep '\[\[vpn\]\]' "$a")" ] && sed -i 's,\[\[vpn\]\],\[\[services\]\],g' "$a"
+    [ -n "$(grep 'admin/vpn' "$a")" ] && sed -i 's,admin/vpn,admin/services,g' "$a"
 done
 
-htm_file="$({ find |grep "\.htm"; } 2>"/dev/null")"
-for b in ${htm_file}
+local resource_file="$({ find | grep "\.lua\|\.htm\|\.json"; } 2>"/dev/null")"
+for b in ${resource_file}
 do
-	[ -n "$(grep '"vpn"' "$b")" ] && sed -i 's,"vpn","services",g' "$b"
-	[ -n "$(grep '"VPN"' "$b")" ] && sed -i 's,"VPN","services",g' "$b"
-	[ -n "$(grep '\[\[vpn\]\]' "$b")" ] && sed -i 's,\[\[vpn\]\],\[\[services\]\],g' "$b"
-	[ -n "$(grep 'admin/vpn' "$b")" ] && sed -i 's,admin/vpn,admin/services,g' "$b"
-done
-
-json_file="$({ find |grep "\.json"; } 2>"/dev/null")"
-for c in ${json_file}
-do
-	[ -n "$(grep '"vpn"' "$c")" ] && sed -i 's,"vpn","services",g' "$c"
-	[ -n "$(grep '"VPN"' "$c")" ] && sed -i 's,"VPN","services",g' "$c"
-	[ -n "$(grep '\[\[vpn\]\]' "$c")" ] && sed -i 's,\[\[vpn\]\],\[\[services\]\],g' "$c"
-	[ -n "$(grep 'admin/vpn' "$c")" ] && sed -i 's,admin/vpn,admin/services,g' "$c"
+    [ -n "$(grep "\"nas\"" "$b")" ] && sed -i "s,\"nas\",\"services\",g" "$b"
+    [ -n "$(grep "\"NAS\"" "$b")" ] && sed -i "s,\"NAS\",\"Services\",g" "$b"
+    [ -n "$(grep "\[\[nas\]\]" "$b")" ] && sed -i "s,\[\[nas\]\],\[\[services\]\],g" "$b"
+    [ -n "$(grep "admin/nas" "$b")" ] && sed -i "s,admin/nas,admin/services,g" "$b"
 done
 
 exit 0
