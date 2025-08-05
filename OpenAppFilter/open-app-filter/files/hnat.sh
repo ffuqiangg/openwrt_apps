075d6e7447748814a6586ddb43be2c77ca45a91b
ffuqiangg <ffuqiangg@gmail.com>
2025-08-05 19:26:18 +0000

    Update 2025/08/05 19:26:18 UTC

--- OpenAppFilter/open-app-filter/files/hnat.sh
+++ OpenAppFilter/open-app-filter/files/hnat.sh
@@ -1,5 +1,3 @@
-. /usr/share/libubox/jshn.sh
-. /lib/functions.sh
 
 disable_hnat=`uci get appfilter.global.disable_hnat`
 
@@ -25,3 +23,54 @@ test -f /etc/config/turboacc && {
     uci -q commit turboacc
     /etc/init.d/turboacc restart &
 }
+
+disable_offload_nat6()
+{
+	if [ "$(uci -q get firewall.@defaults[0].flow_offloading)" = 1 ]; then
+		touch /etc/appfilter/flow_offloading
+		uci -q del firewall.@defaults[0].flow_offloading
+		uci commit firewall
+	fi
+	if [ "$(uci -q get firewall.@defaults[0].flow_offloading_hw)" = 1 ]; then
+		touch /etc/appfilter/flow_offloading_hw
+		uci -q del firewall.@defaults[0].flow_offloading_hw
+		uci commit firewall
+	fi
+	if [ "$(uci -q get firewall.@defaults[0].nat6)" = 1 ]; then
+		touch /etc/appfilter/firewall_nat6
+		uci -q del firewall.@defaults[0].nat6
+		uci commit firewall
+		/etc/init.d/nat6 reload >/dev/null 2>&1
+	fi
+	/etc/init.d/firewall reload >/dev/null 2>&1
+}
+
+enable_offload()
+{
+	rm -f /etc/appfilter/flow_offloading
+	uci -q set firewall.@defaults[0].flow_offloading='1'
+	uci commit firewall
+}
+
+enable_offload_hw()
+{
+	rm -f /etc/appfilter/flow_offloading_hw
+	uci -q set firewall.@defaults[0].flow_offloading_hw='1'
+	uci commit firewall
+}
+
+enable_nat6()
+{
+	rm -f /etc/appfilter/firewall_nat6
+	uci -q set firewall.@defaults[0].nat6='1'
+	uci commit firewall
+	/etc/init.d/nat6 reload >/dev/null 2>&1
+}
+
+if [ "$1" = "restore" ]; then
+	[ -f "/etc/appfilter/flow_offloading" ] && enable_offload
+	[ -f "/etc/appfilter/flow_offloading_hw" ] && enable_offload_hw
+	[ -f "/etc/appfilter/firewall_nat6" ] && enable_nat6
+else
+	disable_offload_nat6
+fi
